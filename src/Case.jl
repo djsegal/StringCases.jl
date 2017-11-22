0d02d975d49da0ba99f87bf6f57c347c592383ad
Dan Segal <danSegal2@gmail.com>
2017-11-22 00:14:26 -0500

    Start Case pkg for adding ruby like string magic

--- src/Case.jl
+++ src/Case.jl
@@ -1,5 +1,91 @@
+__precompile__()
+
 module Case
 
-# package code goes here
+  export upcase, downcase
 
-end # module
+  export spacecase
+
+  export camelcase, pascalcase
+  export snakecase, kebabcase
+
+  export underscore
+
+  upcase = uppercase
+  downcase = lowercase
+
+  function _purgecase(cur_string::AbstractString)
+    cur_string = replace(cur_string, "-", " ")
+    cur_string = replace(cur_string, "_", " ")
+  end
+
+  function _defaultcase(cur_string::AbstractString)
+    cur_string = _purgecase(cur_string)
+
+    cur_string = join(split(cur_string), " ")
+    cur_string = downcase(cur_string)
+
+    cur_string
+  end
+
+  function _delimcase(cur_string::AbstractString, cur_delim::AbstractString)
+    replace(
+      _defaultcase(cur_string),
+      " ",
+      cur_delim
+    )
+  end
+
+  function spacecase(cur_string::AbstractString)
+    _defaultcase(cur_string)
+  end
+
+  function pascalcase(cur_string::AbstractString)
+    replace(
+      titlecase(_defaultcase(cur_string)),
+      " ",
+      ""
+    )
+  end
+
+  function camelcase(cur_string::AbstractString)
+    lcfirst(pascalcase(cur_string))
+  end
+
+  function snakecase(cur_string::AbstractString)
+    _delimcase(cur_string, "_")
+  end
+
+  function kebabcase(cur_string::AbstractString)
+    _delimcase(cur_string, "-")
+  end
+
+  function underscore(cur_string::AbstractString)
+    cur_string = _purgecase(cur_string)
+
+    capital_letters = matchall(r"[A-Z]", cur_string)
+    capital_letters = map(x -> downcase(x), capital_letters)
+
+    lowercase_phrases = split(cur_string, r"([A-Z])")
+
+    cur_array = vcat(
+      map(
+        x -> join(x, "_"),
+        [zip(lowercase_phrases, capital_letters)...]
+      )...
+    )
+
+    cur_string = join(cur_array)
+
+    if length(lowercase_phrases) > length(capital_letters)
+      cur_string *= lowercase_phrases[end]
+    end
+
+    if length(lowercase_phrases) < length(capital_letters)
+      error("Improperly handled string on underscore")
+    end
+
+    snakecase(cur_string)
+  end
+
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,74 @@
 using Case
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+# -------------------
+#  test plain string
+# -------------------
+
+test_string = "foo BAR bAZ"
+
+@test Case._defaultcase(test_string) == "foo bar baz"
+
+@test spacecase(test_string) == "foo bar baz"
+
+@test pascalcase(test_string) == "FooBarBaz"
+
+@test camelcase(test_string) == "fooBarBaz"
+
+@test snakecase(test_string) == "foo_bar_baz"
+
+@test kebabcase(test_string) == "foo-bar-baz"
+
+# -------------
+#  test delims
+# -------------
+
+test_string = "this_var-here"
+
+@test Case._defaultcase(test_string) == "this var here"
+
+@test spacecase(test_string) == "this var here"
+
+@test pascalcase(test_string) == "ThisVarHere"
+
+@test camelcase(test_string) == "thisVarHere"
+
+@test snakecase(test_string) == "this_var_here"
+
+@test kebabcase(test_string) == "this-var-here"
+
+# -----------------
+#  test underscore
+# -----------------
+
+test_string = "foo_bar_baz"
+
+@test underscore("foo_bar_baz") == test_string
+
+@test underscore("foo-bar-baz") == test_string
+
+@test underscore("fooBarBaz") == test_string
+
+@test underscore("FooBarBaz") == test_string
+
+@test underscore("modelA") == "model_a"
+
+@test underscore("ModelA") == "model_a"
+
+@test underscore("aModel") == "a_model"
+
+@test underscore("AModel") == "a_model"
+
+# -------------
+#  test spaces
+# -------------
+
+test_string = "boo"
+
+@test Case._defaultcase("boo") == test_string
+
+@test Case._defaultcase("  boo") == test_string
+
+@test Case._defaultcase("boo  ") == test_string
+
+@test Case._defaultcase(" boo ") == test_string
